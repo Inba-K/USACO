309be457cfc021e05b9d32e8b9ec7c7158c707e8
Inba-K <inba.karthikeyan18@gmail.com>
2026-06-21 18:54:59 -0700

    Update Cowllatz.py
    updated

--- Cowllatz.py
+++ Cowllatz.py
@@ -1,8 +1,8 @@
 def cowllatz(x,j):
-    while x>=1:
-        if x==1:
-            return 1
-        elif x%2==0:
+    if x==1:
+        return 1
+    while x>1:
+        if x%2==0:
             x=x/2
         else:
             x=x*3+1
