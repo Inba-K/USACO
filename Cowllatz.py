17d3548c8cb7baa8f564cd50d4282e3013b2539b
Inba-K <inba.karthikeyan18@gmail.com>
2026-06-21 18:52:03 -0700

    Cowllatz updated
    I updated it to make it faster

--- Cowllatz.py
+++ Cowllatz.py
@@ -1,10 +1,11 @@
-def cowllatz(x):
-    j=0
-    while x!=1:
-        if x%2==0:
+def cowllatz(x,j):
+    while x>=1:
+        if x==1:
+            return 1
+        elif x%2==0:
             x=x/2
         else:
             x=x*3+1
         j+=1
     return j
-print(cowllatz(int(input())))
+print(cowllatz(int(input()),0))
