e64ea3176f24d65aee3ece1e8feb95ced36a6924
Inba-K <inba.karthikeyan18@gmail.com>
2026-07-14 10:38:44 -0700

    Cowdice done
    I got the answer to my USACO warm up

--- Cowdice.py
+++ Cowdice.py
@@ -1,21 +1,16 @@
 a=0
 for i in range(1,5):
     for j in range(1,7):
-        if i>j:
-            break
-        for k in range(1,9):
-            if j>k:
-                break
-            for l in range(1,11):
-                if k>l:
-                    break
-                for m in range(1,13):
-                    if l>m:
-                        break
-                    for n in range(1,2):
-                        if m>n:
-                            break
-                        else:
-                            a+=1
+        if j>=i:
+            for k in range(1,9):
+                if k>=j:
+                    for l in range(1,11):
+                        if l>=k:
+                            for m in range(1,13):
+                                if m>=l:
+                                    for n in range(1,21):
+                                        if n>=m:
+                                            a+=1
+                                            print(i,j,k,l,m,n)
 
-print("Bessie can win in",a,"different ways.")
+print("Bessie can win in",a,"ways.")
