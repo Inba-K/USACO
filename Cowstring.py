2ae0b864e35c17aa5c3003932139b13725787a59
Inba-K <inba.karthikeyan18@gmail.com>
2026-07-17 16:24:12 -0700

    Cowstring done
    I made a program that is supposed to see how many strings of a set length don't contain the forbidden string.

--- Cowstring.py
+++ Cowstring.py
@@ -1,3 +1,13 @@
-def Cowstring(N,list,forbid):
-    for i in range((3**N)-1):
-         
+from itertools import product
+def Cowstring(N,forbid,letters):
+    cowstring=[''.join(item) for item in product(letters,repeat=N)]
+    result=len(cowstring)
+    for i in range(len(cowstring)):
+        if forbid in cowstring[i]:
+            result-=1
+    return result
+
+letters=['C','O','W']
+N,forbid=input().split()
+N=int(N)
+print(Cowstring(N,forbid,letters))
