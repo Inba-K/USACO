4dc8adc33688e7950ef452558741699b7d29a3c9
Inba-K <inba.karthikeyan18@gmail.com>
2026-07-19 16:36:42 -0700

    a
    a

--- MOOdoku.py
+++ MOOdoku.py
@@ -1,5 +1,5 @@
 def MOOdoku(str1,str2,str3,str4,str5,str6,M):
-    for i in range(6):
+    for i in range(0,5):
         if str1[i]=='M':
             M+=1
         elif str2[i]=='M':
@@ -16,7 +16,14 @@ def MOOdoku(str1,str2,str3,str4,str5,str6,M):
             M=0
             return 0
         else:
-            return "Good"
+            for j in str_list:
+                if j[i]=='.' and M<2:
+                    j[i].replace('.','M')
+                elif j[i]=='.' and M==2:
+                    j[i].replace('.','O')
+                print(j)
+
+        
         
 
 str1=input().split()
@@ -26,4 +33,5 @@ str4=input().split()
 str5=input().split()
 str6=input().split()
 M=0
+str_list=[str1,str2,str3,str4,str5,str6]
 print(MOOdoku(str1,str2,str3,str4,str5,str6,M))
